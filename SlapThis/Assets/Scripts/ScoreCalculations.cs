cde6d6e0b997279dcdf03e08d01df00090232c93
Manuel Munoz <manuelmunozr.92@gmail.com>
2017-01-21 18:22:07 -0400

    added PowerMeter

--- SlapThis/Assets/Scripts/ScoreCalculations.cs
+++ SlapThis/Assets/Scripts/ScoreCalculations.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCalculations : MonoBehaviour {
 
+	// User controls broadcaster
 	public InputController inputController;
+	// Points for every action
 	public int pleasurePoints;
 	public int hitSuccessPointsAmount;
 	public int hitFailReducePointsAmount;
+	// Pleasure meter: Score feedback meter
+	public Slider pleasureSlider;
+	// Force meter: How much point does it earns when slap the butt
+	public Slider powerSlider;
 
 	// Use this for initialization
 	void Start () {
@@ -30,7 +37,6 @@ public class ScoreCalculations : MonoBehaviour {
 			// When touched a slap point
 			if (hit.collider.CompareTag (Tags.SLAP_POINT)) {
 				Destroy (hit.transform.gameObject);
-				Debug.Log ("Acabo de nalguear");
 				ReducePoints (-hitSuccessPointsAmount);
 			}
 			// When missed a slap point, reduce excite points
@@ -42,11 +48,23 @@ public class ScoreCalculations : MonoBehaviour {
 		}
 	}
 
+	// Method for points loss
 	void ReducePoints(int amount){
 		pleasurePoints -= amount;
+
+		pleasureSlider.value = pleasurePoints;
 
 		if(pleasurePoints <= 0) {
 			//TODO: FRIENDZONE DUDE
 		}
+	}
+
+	// Method for points gain
+	void IncresasePoints() {
+		// Get the amount of the slap's power
+		float power = powerSlider.value;
+
+		// TODO CREAR LA CONVERSION DE FUERZA A PUNTOS
+
 	}
 }
