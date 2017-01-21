61822d02ccceefeacf370ce9bc15aa2151d5941c
Manuel Munoz <manuelmunozr.92@gmail.com>
2017-01-21 16:16:26 -0400

    Added Tags class to handle tag literals

---
+++ SlapThis/Assets/Scripts/Tags.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tags {
+	public const string CULITO = "Culito";
+	public const string SLAP_POINT = "SlapPoint";
+}
