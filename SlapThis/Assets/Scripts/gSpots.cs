5032e43fa0b981c4008537b7f6d615402b7467e5
Manuel Munoz <manuelmunozr.92@gmail.com>
2017-01-21 16:17:57 -0400

    If a slap point gets automatically destroyed you lose some points

--- SlapThis/Assets/Scripts/gSpots.cs
+++ SlapThis/Assets/Scripts/gSpots.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class gSpots : MonoBehaviour {
 
+	public delegate void ReducePointsEvent(int amount);
+	public static event ReducePointsEvent OnSlapDestroyed;
+
 	// Butt gameobject
 	public Transform butt;
 	public float slapRadius;
+	public int destroyPointAmount;
+
+	// Slap points prefab
+	[Tooltip("Slap point to be hit by the user")]
+	public GameObject slapPoint;
 
 	void Start () {
 		StartCoroutine (CreateSlapPoints());
-	}
-
-	void FixedUpdate () {
-		
 	}
 
 	IEnumerator CreateSlapPoints() {
@@ -23,18 +27,30 @@ public class gSpots : MonoBehaviour {
 
 			RaycastHit hit;
 			if (Physics.Raycast(transform.position,directionVector * 100, out hit)) {
-				if (hit.collider.CompareTag ("Culito")) {
+				if (hit.collider.CompareTag (Tags.CULITO)) {
 					Debug.DrawRay (transform.position, directionVector);
+					GameObject slapObj = Instantiate (slapPoint, hit.point - directionVector * 0.1f, Quaternion.identity) as GameObject;
+//
+					this.InvokeAfterSeconds(3, () => {
+						if(slapObj == null) return;
 
-					GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-					Vector3 spherePosition = hit.point - directionVector * 0.1f;
-					sphere.transform.position = spherePosition;
-					sphere.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+						Destroy(slapObj);
+						// TODO QUITAR PUNTOS
+						if(OnSlapDestroyed != null) OnSlapDestroyed(destroyPointAmount);
+					});
+//					GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+//					Vector3 spherePosition = hit.point - directionVector * 0.1f;
+//					sphere.transform.position = spherePosition;
+//					sphere.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+//					sphere.tag = Tags.SLAP_POINT;
 
 					// Return to frame renderer
 					yield return new WaitForSeconds(2);
 				}
 			}
+
 		}
 	}
+
+
 }
