8078f1451a723975505af517040c7b252710fb16
David Marull <david.marull@gmail.com>
2017-01-22 22:22:34 -0400

    Add simple AudioManager

---
+++ SlapThis/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager instance;
+
+    //Singleton
+    public static AudioManager Instance {
+        get {
+            if( instance == null ) instance = FindObjectOfType<AudioManager>();
+            return instance;
+        }
+        set {
+            instance = value;
+        }
+    }
+
+    public AudioSource[] slaps;
+    public AudioSource backgroundMusic;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
